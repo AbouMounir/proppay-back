2b2036e261776e86ce2ecbcb49f585860c939ae4
kouyate karim <kouyatekarim02@gmail.com>
2024-06-12 11:40:43 +0000

    restructuring

--- models/Propriety.js
+++ models/Propriety.js
@@ -16,15 +16,30 @@ const Propriety = mongoose.model('proprietes', {
         type: String,
         /* required: [true, "The Propriety type is required"], */
     },
-    proprietyImages: String,
+    proprietyImages: {
+        type : String,
+        default : ""
+    },
     proprietyOccupation: {
         type: String,
         /* required: [true, "'Is a propriety' occupied is requiried"] */
     },
-    proofOfPropriety: String,
-    totalUnits: String,
-    occupiedUnits: String,
-    availableUnits: String,
+    proofOfPropriety: {
+        type : String,
+        default : ""
+    },
+    totalUnits: {
+      type :   String,
+      default : 0
+    },
+    occupiedUnits:  {
+        type :   String,
+        default : 0
+      },
+    availableUnits:  {
+        type :   String,
+        default : 0
+      },
     listOfTenants:  [
         {
         type: mongoose.Schema.Types.ObjectId,
